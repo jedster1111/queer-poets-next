05c34d5647621282ffe27995f59cbd3870348ad9
Jed Thompson <jedster1111@hotmail.co.uk>
2021-02-13 05:33:46 +0000

    Tidy up github pages action

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   webpack: (cfg) => {
+    cfg.basePath = '/queer-poets-next'
+    cfg.assetPrefix = '/queer-poets-next/'
     cfg.module.rules.push({
       test: /\.md$/,
       loader: "frontmatter-markdown-loader",
