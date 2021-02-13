647860aeffd71144b2b5f36f6cccd51b039e0fd7
Jed Thompson <jedster1111@hotmail.co.uk>
2021-02-13 06:01:50 +0000

    Remove broken config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   webpack: (cfg) => {
-    cfg.basePath = '/queer-poets-next'
-    cfg.assetPrefix = '/queer-poets-next/'
     cfg.module.rules.push({
       test: /\.md$/,
       loader: "frontmatter-markdown-loader",
